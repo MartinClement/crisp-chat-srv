97fc19ae73734bf322994dd0ce33850493045755
MartinClement <martinclementkjules@gmail.com>
2023-10-26 09:53:28 +0200

    quick setup: server + socket

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+});
+
+const port = 1337;
+
+app.get('/', (req, res) => {
+  res.send('Express + Typescript Server');
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
