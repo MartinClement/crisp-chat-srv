25364ec7aafa7c77ffdd22924586fec464820256
MartinClement <martinclementkjules@gmail.com>
2023-10-31 09:05:44 +0100

    create env file and use it

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import http from "http";
 
 import addRoomListeners from "./listeners.ts/roomListeners";
 import { RoomStorage } from "./storage/RoomStorage";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
-  cors: { origin: "*" },
+  ...(process.env.MODE === 'dev' ? { cors: { origin: "*" } } : {}),
 });
 
-const port = 1337;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Express + Typescript Server');
